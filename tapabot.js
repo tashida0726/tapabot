94d10293c903a61500707cfc5015c8c3764f63cb
Ashida <ashida@jp.ibm.com>
2021-07-17 16:49:17 +0900

    Add logger

--- tapabot.js
+++ tapabot.js
@@ -1,32 +1,36 @@
+const winston = require('winston')
 const express = require('express');
 const bodyParser = require('body-parser');
 const querystring = require('querystring');
 const discord = require('discord.js');
 const client = new discord.Client();
 
-const app = express();
+var utime = Date();
 
+const format = winston.format
+const logger = winston.createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.cli(),
+        format.printf(info => `${info.timestamp} ${info.level} ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.File({filename: '/var/log/tapabot.log', level: 'info'})
+    ]
+});
+
+const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(bodyParser.json());
-
-var stocks = {
-    "AMZN" : {
-        "price" : 100
-    }
-}
-var updatedTime = Date();
-
 app.post("/stocks", (req, res) => {
+    logger.info('New stocks info arrived.')
     stocks = req.body;
-    updatedTime = Date();
-    console.log(updatedTime.toString()+" price="+stocks["AMZN"]["price"])
 
     res.send("OK")
 });
-
 app.listen(8080)
 
 client.on('ready', message =>{
@@ -38,11 +42,15 @@ client.on('message', message =>{
     if (message.author.id == client.user.id){
         return;
     }
-    if(message.isMemberMentioned(client.user)){
-        sendReply(message, "tapabotたぱ");
-        return;
+    if (message.mentions.members.has(client.user.id)) {
+        console.log("Yes")
+    } else {
+        console.log("No")
     }
+    console.log(message.member)
     let tok = message.content.split("$")
+    console.log(client.user.id)
+    console.log(tok[0])
     if(tok.length == 2) {
         let ticker = tok[1].toUpperCase()
         let msg = "語る会銘柄["+ticker+"]について語るたぱ\n"
@@ -50,7 +58,8 @@ client.on('message', message =>{
         msg += "現在値($):\t3401.46\n"
         msg += "見込み値($):\t5312.50\n"
         msg += "乖離率(%):\t64.0\n"
-        msg += "AWS プライム 通販 起業家オーナー◎ 売り上げ9倍 営利 5%"
+        msg += "AWS プライム 通販 起業家オーナー◎ 売り上げ9倍 営利 5%\n"
+        msg += "[Chart]https://finance.yahoo.com/quote/"+ticker+"/chart"
         sendMsg(message.channel.id, msg);
         return;
     }
